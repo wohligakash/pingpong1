4f6eaf94970e4e10b9fc792cfb4d7fe24ab171a7
wohligakash <akash.pawar@wohlig.com>
2023-01-30 18:34:51 +0530

    deliberate error

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-function calculateSum(a, b) {
+functi calculateSum(a, b) {
     var sum = a + b;
     return sum;
 }
-  
-<<<<<<< HEAD
-var result = calculateSum(1, 4);
-=======
+
 var result = calculateSum(2, 3);
->>>>>>> parent of 015590e (changes)
 console.log(result);
 
 app.get('/', (req,res) => {
